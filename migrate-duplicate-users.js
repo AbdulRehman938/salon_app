80bc8321cc4649ef4d9c299236dd1c6790c3906a
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2026-05-05 11:49:32 +0500

    vercel build solve error 15

--- migrate-duplicate-users.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-// Repo-root launcher so this works:
-// node migrate-duplicate-users.js [--apply] [--delete-duplicates]
-require('./functions/scripts/migrate-duplicate-users.js');
